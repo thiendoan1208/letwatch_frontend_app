d987e87517befd735f34d635e34d5782b5558e85
thiendoan1208 <thienmacro1234@gmail.com>
2025-07-30 14:37:44 -0400

    test api request

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BACKEND_URL,
+  baseURL: "https://api.letwatch.net/api",
+  withCredentials: true,
 });
-
-instance.defaults.withCredentials = true;
 
 // Add a request interceptor
 instance.interceptors.request.use(
